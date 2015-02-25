626b412faeada40085375b11e70c8fc9a395178f
briansteffes <brian@steffes.io>
2015-02-25 14:46:08 -0600

    hello world file

---
+++ hello.py
@@ -0,0 +1,14 @@
+"""Notes about syntax:
+		*	automatically includes a new line character '\n'
+		*	parenthesis unecessary to use the print function
+		*	libraries are already accessible to the compiler w/ out a specific 'include <...>'
+		*	Return key 'Enter' signifies end of use of the function.
+				- No need for end of line character ';'
+				- How would I be able to span the contents of a funciton across multiple lines?
+
+				You need to explicitly change the default newline character to a space or a tab.
+"""
+
+String name = 'Brian'
+
+
